b2e069b5eea3500ebab67f15de19997147282b3a
David McDonald <davemcdonald@live.co.uk>
2018-10-31 14:39:42 +0000

    nfgr

--- remove_edges_script.sh
+++ remove_edges_script.sh
@@ -6,14 +6,15 @@
 #SBATCH --time=3-00:00:00
 #SBATCH --ntasks=1
 #SBATCH --mem=64G
-#SBATCH --array 0-239
+#SBATCH --array 0-119
 
-DATA_DIR=/rds/projects/2018/hesz01/data
+# DATA_DIR=/rds/projects/2018/hesz01/data
+DATA_DIR="data"
 
 module purge; module load bluebear
 module load apps/python3/3.5.2
 
-ARR=(--dataset={cora_ml,cora,pubmed,citeseer,AstroPh,CondMat,GrQc,HepPh}" "--seed={0..29})
+ARR=(--dataset={cora_ml,cora,pubmed,citeseer}" "--seed={0..29})
 
 echo "beginning "${ARR[${SLURM_ARRAY_TASK_ID}]}
 python remove_edges.py ${ARR[${SLURM_ARRAY_TASK_ID}]} --data-directory ${DATA_DIR}
