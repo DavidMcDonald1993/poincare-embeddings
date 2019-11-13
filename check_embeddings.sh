f2a722a1917014296c6138488b3f4795ffb1c3c1
davemcdonald@live.co.uk <davemcdonald@live.co.uk>
2019-11-13 13:49:17 +0000

    added chece embedding

---
+++ check_embeddings.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+for dataset in cora_ml citeseer ppi pubmed mit
+do
+    for exp in nc_experiment lp_experiment
+    do
+        for seed in {0..29}
+        do
+            for dim in 5 10 25 50
+            do
+                embedding_f=$(printf "embeddings/${dataset}/dim=%02d/seed=%03d/${exp}/embedding.csv" ${dim} ${seed})
+                if [ ! -f ${embedding_f} ]
+                then 
+                    echo no embedding at ${embedding_f}
+                fi
+            done
+        done
+    done
+done
