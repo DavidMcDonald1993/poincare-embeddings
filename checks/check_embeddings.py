77c6d660a4c80de9f9a67aa483d3e5e7b1a01d4e
davemcdonald@live.co.uk <davemcdonald@live.co.uk>
2020-02-06 09:01:55 +0000

    added check script

--- checks/check_embeddings.py
+++ checks/check_embeddings.py
@@ -7,9 +7,8 @@ from pandas.errors import EmptyDataError
 def main():
 
     datasets = ["cora_ml", "citeseer", "ppi", "pubmed", "mit"]
-    dims = ["5", "10", "25", "50"]
-    # methods = ["abrw", "attrpure", "deepwalk", "tadw", "aane", "sagegcn"]
-    seeds = [str(x) for x in range(30)]
+    dims = (5, 10, 25, 50)
+    seeds = range(30)
     exps = ["nc_experiment", "lp_experiment"]
 
     for dataset, dim, seed, exp in itertools.product(
